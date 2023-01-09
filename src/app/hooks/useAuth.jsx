08a8ea29fc2e603e0d0f9bf1a814e7d55a201751
Aleksander <asava@sibmail.com>
2023-01-09 18:05:04 +0700

    ok

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import axios from 'axios'
 
+const httpAuth = axios.create()
 const AuthContext = React.createContext()
 
 export const useAuth = () => {
@@ -12,7 +13,7 @@ const AuthProvider = ({ children }) => {
     async function singUp({ email, password }) {
         const key = 'AIzaSyBZ7tEUa5PXUm14BMImCqvLqPtFwnwHOko'
         const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`
-        const { data } = await axios.post(url, {
+        const { data } = await httpAuth.post(url, {
             email,
             password,
             returnSecureToken: true
