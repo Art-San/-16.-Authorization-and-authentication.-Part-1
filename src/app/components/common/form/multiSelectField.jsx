2fe5f48458c79390daed40c094c4f8b1d78354f6
Aleksander <asava@sibmail.com>
2023-01-09 14:35:04 +0700

    создали базу для Auth-Provider

--- src/app/components/common/form/multiSelectField.jsx
+++ src/app/components/common/form/multiSelectField.jsx
@@ -3,17 +3,17 @@ import Select from 'react-select'
 import PropTypes from 'prop-types'
 
 const MultiSelectField = ({ options, onChange, name, label, defaultValue }) => {
-    // const optionsArray =
-    //     !Array.isArray(options) && typeof options === 'object'
-    //         ? Object.values(options)
-    //         : options
     const optionsArray =
         !Array.isArray(options) && typeof options === 'object'
-            ? Object.keys(options).map((optionsName) => ({
-                label: options[optionsName].name,
-                value: options[optionsName]._id
-            }))
+            ? Object.values(options)
             : options
+    // const optionsArray =
+    //     !Array.isArray(options) && typeof options === 'object'
+    //         ? Object.keys(options).map((optionsName) => ({
+    //             label: options[optionsName].name,
+    //             value: options[optionsName]._id
+    //         }))
+    //         : options
 
     const handleChange = (value) => {
         onChange({ name: name, value })
--- src/app/components/ui/registerForm.jsx
+++ src/app/components/ui/registerForm.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { validator } from '../../utils/validator'
 import TextField from '../common/form/textField'
-// import api from "../../api"
 import SelectField from '../common/form/selectField'
 import RadioField from '../common/form/radioField'
 import MultiSelectField from '../common/form/multiSelectField'
@@ -60,23 +59,6 @@ const RegisterForm = () => {
     //     return qualitiesArray
     // }
 
-    // useEffect(() => {
-    //     // api.professions.fetchAll().then((data) => {
-    //     //     const professionsList = Object.keys(data).map((professionName) => ({
-    //     //         label: data[professionName].name,
-    //     //         value: data[professionName]._id
-    //     //     }))
-    //     //     setProfession(professionsList)
-    //     // })
-    //     // api.qualities.fetchAll().then((data) => {
-    //     //     const qualitiesList = Object.keys(data).map((optionName) => ({
-    //     //         value: data[optionName]._id,
-    //     //         label: data[optionName].name,
-    //     //         color: data[optionName].color
-    //     //     }))
-    //     //     setQualities(qualitiesList)
-    //     // })
-    // }, [])
     const handleChange = (target) => {
         setData((prevState) => ({
             ...prevState,
