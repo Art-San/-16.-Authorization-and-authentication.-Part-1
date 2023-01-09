44d67c6e5db7b63a7c61cf1ebec27c243298e210
Aleksander <asava@sibmail.com>
2023-01-10 00:11:42 +0700

    неработает localStorage

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -1,6 +1,8 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { toast } from 'react-toastify'
 import axios from 'axios'
+import userService from '../services/user.service'
 
 const httpAuth = axios.create()
 const AuthContext = React.createContext()
@@ -8,20 +10,57 @@ const AuthContext = React.createContext()
 export const useAuth = () => {
     return useContext(AuthContext)
 }
+const TOKEN_KEY = 'jwt-token'
+const REFRESH_KEY = 'jwt-refresh-token'
+const EXPIRES_KEY = 'jwt-expires'
 
 const AuthProvider = ({ children }) => {
-    async function singUp({ email, password }) {
+    const [currentUser, setUser] = useState({})
+    const [error, setError] = useState(null)
+    function setTokens({ refreshToken, idToken, expiresIn = 3600 }) {
+        const expiresDate = new Date().getTime() + expiresIn * 1000
+        localStorage.setItem(TOKEN_KEY, idToken)
+        localStorage.setItem(REFRESH_KEY, refreshToken)
+        localStorage.setItem(EXPIRES_KEY, expiresDate)
+    }
+    async function singUp({ email, password, ...rest }) {
         const key = 'AIzaSyBZ7tEUa5PXUm14BMImCqvLqPtFwnwHOko'
         const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`
-        const { data } = await httpAuth.post(url, {
-            email,
-            password,
-            returnSecureToken: true
-        })
-        console.log('data', data)
+        try {
+            const { data } = await httpAuth.post(url, {
+                email,
+                password,
+                returnSecureToken: true
+            })
+            console.log('data', data)
+            setTokens(data)
+            await createUser({ _id: data.localId, email, ...rest })
+        } catch (error) {
+            errorCatcher(error)
+        }
     }
+
+    async function createUser(data) {
+        try {
+            const { content } = userService.create(data)
+            setUser(content)
+        } catch (error) {
+            errorCatcher(error)
+        }
+    }
+
+    function errorCatcher(error) {
+        const { message } = error.response.data
+        setError(message)
+    }
+    useEffect(() => {
+        if (error !== null) {
+            toast(error)
+            setError(null)
+        }
+    }, [error])
     return (
-        <AuthContext.Provider value={{ singUp }}>{children}</AuthContext.Provider>
+        <AuthContext.Provider value={{ singUp, currentUser }}>{children}</AuthContext.Provider>
     )
 }
 
